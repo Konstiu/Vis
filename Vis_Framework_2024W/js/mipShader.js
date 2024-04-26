10c563514db81ef00c4993d410207d1365ec2ef4
Niklas Gruener 12217059 <e12217059@student.tuwien.ac.at>
2024-04-26 18:18:07 +0200

    Vamos 2!!

--- Vis_Framework_2024W/js/mipShader.js
+++ Vis_Framework_2024W/js/mipShader.js
@@ -1,10 +1,22 @@
 
 
 class MipShader extends Shader {
-    constructor(color) {
+    constructor() {
         super("mip_vert", "mip_frag");
-        this.setUniform("color",
-                new  THREE.Vector4(color[0], color[1], color[2], color[3]),
-            "vec4");
+    }
+
+    setVolume(volume) {
+        const texture = new THREE.Data3DTexture(volume.voxels, volume.width, volume.height, volume.depth);
+        texture.format = THREE.RedFormat;
+        texture.type = THREE.FloatType;
+        texture.minFilter = texture.magFilter = THREE.LinearFilter;
+        texture.unpackAlignment = 1;
+        texture.needsUpdate = true;
+
+        this.setUniform("volume", texture);
+        this.setUniform("volume_dims", new THREE.Vector3(volume.width, volume.height, volume.depth));
+        this.setUniform("volume_scale", new THREE.Vector3(volume.width/volume.max, volume.height/volume.max, volume.depth/volume.max));
+
+
     }
 }
