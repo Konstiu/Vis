9ac290d0101fcb815a89313ef1235eefdd845e93
12217059 <e12217059@tuwien.ac.at>
2024-06-01 19:39:41 +0200

    moved gradient calculation to shader

--- Vis_Framework_2024W/js/firsHitShader.js
+++ Vis_Framework_2024W/js/firsHitShader.js
@@ -19,20 +19,9 @@ class FirstHitShader extends Shader {
         texture.unpackAlignment = 1;
         texture.needsUpdate = true;
 
-        const gradient = volume.calcGradient();
-
-        const gradient_texture = new THREE.Data3DTexture(gradient, volume.width, volume.height, volume.depth);
-        gradient_texture.type = THREE.FloatType;
-        gradient_texture.format = THREE.RGBAFormat;
-        gradient_texture.minFilter = texture.magFilter = THREE.LinearFilter;
-        gradient_texture.unpackAlignment = 4;
-        gradient_texture.needsUpdate = true;
-        this.setUniform("gradient", gradient_texture);
 
         this.setUniform("volume", texture);
         this.setUniform("volume_scale", volume.scale);
-        this.setUniform("iso_value", 0.3);
-        this.setUniform("surface_color", new THREE.Vector4(1, 1, 1));
     }
 
     /**
